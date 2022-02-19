f9550bf0e7bf5ee051d978202a2d1eb228da68f3
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-19 05:41:22 -0500

    Connected to the database.

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 
+const mysql = require("mysql2");
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username
+    user: "root",
+    // Your MySQLpassword
+    password: "*?x27>,vz9&@;tT9",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query("SELECT * FROM candidates", (err, rows) => {
+  console.log(rows);
+});
 
 // Default response to everything else
 app.use((req, res) => {
