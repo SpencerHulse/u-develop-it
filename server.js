8c7ad61277d80338221c216a270bbe0cb89b2708
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-20 06:04:21 -0500

    Joined the candidates and parties tables in the GET routes.

--- server.js
+++ server.js
@@ -37,7 +37,13 @@ db.query(sql, params, (err, result) => {
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `
+  SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id
+  `;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -52,7 +58,14 @@ app.get("/api/candidates", (req, res) => {
 
 // Get one candidate
 app.get("/api/candidates/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `
+  SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?
+  `;
   const params = req.params.id;
 
   db.query(sql, params, (err, row) => {
