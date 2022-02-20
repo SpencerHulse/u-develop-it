9563d699db645695bf28abf17c42be63dc4b4532
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-19 21:49:34 -0500

    Added delete route for a single candidate.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const mysql = require("mysql2");
+const { restart } = require("nodemon");
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -37,7 +38,7 @@ db.query("DELETE FROM candidates WHERE id = ?", 1, (err, result) => {
   }
 }); */
 
-// Create a candidate
+/* // Create a candidate
 const sql =
   "INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)";
 const params = [1, "Ronald", "Firbank", 1];
@@ -48,6 +49,62 @@ db.query(sql, params, (err, result) => {
   } else {
     console.log(result);
   }
+});
+ */
+
+// Get all candidates
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      // Server error (500)
+      res.status(500).json({ error: err.message });
+      return;
+    } else {
+      res.status(200).json({ message: "success", data: rows });
+    }
+  });
+});
+
+// Get one candidate
+app.get("/api/candidates/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = req.params.id;
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      // Request not accepts, try a different one (400)
+      // because it's the specific id not working
+      res.status(400).json({ error: err.message });
+      return;
+    } else {
+      res.status(200).json({ message: "success", data: row });
+    }
+  });
+});
+
+// Delete a candidate
+app.delete("/api/candidates/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = req.params.id;
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    } else if (!result.affectedRows) {
+      res.json({ message: "Candidate not found!" });
+    } else {
+      res
+        .status(200)
+        .json({
+          message: "deleted",
+          changes: result.affectedRows,
+          id: req.params.id,
+        });
+    }
+  });
 });
 
 // Default response to everything else
