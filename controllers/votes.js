814c2a314052d7723d4bb80a6d0ed36486bdf9e8
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-20 22:28:42 -0500

    Added files for votes.

---
+++ controllers/votes.js
@@ -0,0 +1,4 @@
+const db = require("../db/connection");
+const inputCheck = require("../utils/inputCheck");
+
+module.exports = {};
--- routes/api_routes/index.js
+++ routes/api_routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const candidatesRoutes = require("../../routes/api_routes/candidates");
 const partiesRoutes = require("../../routes/api_routes/parties");
 const votersRoutes = require("../../routes/api_routes/voters");
+const votesRoutes = require("../../routes/api_routes/votes");
 
 router.use(candidatesRoutes);
 router.use(partiesRoutes);
 router.use(votersRoutes);
+router.use(votesRoutes);
 
 module.exports = router;
